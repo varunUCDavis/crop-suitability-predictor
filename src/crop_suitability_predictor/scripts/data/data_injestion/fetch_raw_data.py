d55ab1c38237ee0c62a7da60e4cbdb91f48e3def
varunUCDavis <vwadhwa@ucdavis.edu>
2025-05-20 13:49:38 -0700

    Added funtionality to fetch soil and ndvi data

--- src/crop_suitability_predictor/scripts/data/data_injestion/fetch_raw_data.py
+++ src/crop_suitability_predictor/scripts/data/data_injestion/fetch_raw_data.py
@@ -1,25 +1,18 @@
 import os 
-from crop_suitability_predictor.modules.data.data_injestion_utils import CDLRasterStatus, check_if_cdl_raster_exists, download_cdl_raster, submit_cdl_clip_job, unzip_file
-from crop_suitability_predictor.config.config import PROJECT_PATH, RAW_DATA_PATH, CDL_RASTER_FOLDER_NAME, CDL_ZIP_FILE_NAME, CDL_RASTER_PAYLOAD_NAME, RUNTIME_CONFIG_PATH
+from crop_suitability_predictor.modules.data.data_injestion_utils import fetch_CA_shape_file, fetch_cdl_rasters, fetch_ndvi_tiles, fetch_soil_rasters
+
+
+
+
 
 
 def fetch_raw_data():
-    breakpoint()
-    # check if cdl rasters exist
-    cdl_zip_file_path = os.path.join(RAW_DATA_PATH, CDL_RASTER_FOLDER_NAME,CDL_ZIP_FILE_NAME)
-    cdl_extracted_folder_path = os.path.join(RAW_DATA_PATH, CDL_RASTER_FOLDER_NAME)
-    cdl_extracted_file_path = os.path.join(cdl_extracted_folder_path,"clipped", "clipped.TIF")
-    cdl_raster_status = check_if_cdl_raster_exists(cdl_extracted_raster_path=cdl_extracted_file_path, cdl_zip_file_path=cdl_zip_file_path)
-    cdl_raster_payload_path = os.path.join(RUNTIME_CONFIG_PATH, CDL_RASTER_PAYLOAD_NAME)
-    if cdl_raster_status == CDLRasterStatus.NOTHING_EXISTS:
-        download_url = submit_cdl_clip_job(json_payload_path=cdl_raster_payload_path)
-        download_cdl_raster(url=download_url, zip_path=cdl_extracted_folder_path, extraction_path=cdl_extracted_file_path)
-    elif cdl_raster_status == CDLRasterStatus.ZIP_EXISTS:
-        # extract zip file
-        unzip_file(zip_path=cdl_zip_file_path, output_folder=cdl_extracted_folder_path)
-        
-        
-    # check if 
+    # breakpoint()
+    fetch_cdl_rasters()
+    fetch_CA_shape_file()
+    fetch_ndvi_tiles()
+    fetch_soil_rasters()  
+    
 
 def main() -> None:
     fetch_raw_data()
